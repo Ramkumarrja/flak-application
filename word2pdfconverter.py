e592af63c9c80202cb9ea8711b913216c5a71b6d
ramkumarraja <ramkumarraja240@gmail.com>
2023-12-13 02:33:07 +0530

    created subprocess code

--- word2pdfconverter.py
+++ word2pdfconverter.py
@@ -1,5 +1,5 @@
 from docx2pdf import convert
-
+import subprocess
 class Docx2PdfConverter:
     def __init__(self, word,pdf):
         self.word_file = word
@@ -8,4 +8,18 @@ class Docx2PdfConverter:
     def word2pdf_converter(self):
         # Convert the DOCX file to PDF
         return convert (self.word_file,self.pdf_file)
-        # No need to return anything here, the file is sent within a Flask route
+        # No need to return anything here, the file is sent within a Flask rout
+    
+    def convert_docx_to_pdf_linux(self):
+        """
+        Converts the input DOCX file to a PDF file.
+
+        Returns:
+        - bool: True if the conversion is successful, False otherwise.
+        """
+        try:
+            subprocess.run(["libreoffice", "--headless", "--convert-to", "pdf", self.docx_path, "--outdir", "output_folder"])
+            return True
+        except subprocess.CalledProcessError as e:
+            print(f"Error during conversion: {e}")
+            return False
